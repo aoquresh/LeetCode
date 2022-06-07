9608d8990528ce216f52f10998eafb25825797a2
aoquresh <aoquresh@gmail.com>
2022-06-07 16:49:55 -0500

    Best time to buy and sell stock

---
+++ Blind 75/Arrays/Easy/121. Best Time to Buy and Sell Stock/List_O(n).py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        
+        bought = prices[0]
+        maxProf = 0
+        
+        for i in prices:
+            if i < bought:
+                bought = i
+            
+            elif i - bought > maxProf:
+                maxProf = i - bought
+        return maxProf
+        
